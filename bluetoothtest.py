3f7c8632e1137163247b25d57bbe44c5aed747b9
Abhay D <a.deshpande012@gmail.com>
2018-07-26 14:27:02 -0700

    Added optional timeout

--- bluetoothtest.py
+++ bluetoothtest.py
@@ -1,8 +1,13 @@
 from __future__ import print_function
-
+import sys
 import serial
 
-conn = serial.Serial('/dev/rfcomm0')
+if len(sys.argv) > 1:
+    timeout=sys.argv[1]
+else:
+    timeout=100
+
+conn = serial.Serial('/dev/rfcomm0',timeout=timeout)
 print('Connected to bluetooth')
 
 while True:
