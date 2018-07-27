4ccf70b8df74eabc320eac540a6db7f927c2ab5c
Abhay D <a.deshpande012@gmail.com>
2018-07-26 23:41:57 -0700

    Remove bluetooth stuff

--- bluetoothtest.py
+++ bluetoothtest.py
@@ -1,14 +1,1 @@
-from __future__ import print_function
-import sys
-import serial
-
-if len(sys.argv) > 1:
-    timeout=int(sys.argv[1])
-else:
-    timeout=100
-
-conn = serial.Serial('/dev/rfcomm0',timeout=timeout)
-print('Connected to bluetooth')
-
 while True:
-    print(conn.readline())
