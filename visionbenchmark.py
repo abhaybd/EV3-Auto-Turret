60b6bc2902582d4705133cb99c052a1a56578303
Abhay D <a.deshpande012@gmail.com>
2018-07-30 15:36:01 -0700

    Added visionbenchmark

---
+++ visionbenchmark.py
@@ -0,0 +1,24 @@
+from __future__ import print_function, division
+from imageprocessor import process
+from camera import Camera
+import time
+
+X_FOV = 35.9
+Y_FOV = 27.3
+
+vision = Camera(process, X_FOV, Y_FOV)
+print('Camera resolution: %s' % str(vision.resolution()))
+
+fps_sum = 0
+readings = 0
+
+while True:
+    before = time.time()
+    vision.get_vision_frame()
+    after = time.time()
+    
+    elapsed = after - before
+    fps = 1.0/elapsed
+    fps_sum += fps
+    readings += 1
+    print('\rFPS: %.1f, Avg. FPS: %.1f' % (fps, fps_sum / readings), end='')
